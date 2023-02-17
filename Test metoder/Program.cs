273461d0107e7ee520fc285074006cd509da3dac
Suberhaibe <suha05002@utb.vaxjo.se>
2023-02-17 10:32:47 +0100

    Add project files.

---
+++ Test metoder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static int AREA(int höjd, int bredd)
+    {
+        return höjd * bredd;
+    }
+    static void Main()
+    {
+        Console.Write("Skriv höjden för din rektangen " );
+        int Höjd = int.Parse(Console.ReadLine());
+        Console.Write("Skriv bredden för rektangen  " );
+        int bredd = int.Parse(Console.ReadLine());
+
+        int area = AREA(Höjd, bredd);
+        Console.WriteLine("Arean på rektanglen är " + area);
+    }
+}
